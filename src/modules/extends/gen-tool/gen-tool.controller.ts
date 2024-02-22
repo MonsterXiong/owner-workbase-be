dfd43bfe52fd54d1eca6189942c97bd017fdd007
MonsterXiong <942849672@qq.com>
2024-02-22 20:24:12 +0800

    update

--- src/modules/extends/gen-tool/gen-tool.controller.ts
+++ src/modules/extends/gen-tool/gen-tool.controller.ts
@@ -3,63 +3,69 @@ import { GenToolService } from './gen-tool.service';
 import { ApiOperation, ApiProperty, ApiTags } from '@nestjs/swagger';
 import { quickGenAdapter } from '../../../../submodule/genCode-utils/src/tool/quickGenAdapter/quickGenAdapter';
 
-class elementDto{
+class elementDto {
   @ApiProperty({
     description: '字段key rowInfo',
-    required:true,
-    default:''
+    required: true,
+    default: '',
   })
-  field:string
+  field: string;
 
   @ApiProperty({
     description: '要素名称 queryRow',
-    required:true,
-    default:''
+    required: true,
+    default: '',
   })
-  elementName:string
-
+  elementName: string;
 
   @ApiProperty({
     description: '要素信息 xx信息',
-    required:true,
-    default:''
+    required: true,
+    default: '',
   })
-  message:string
+  message: string;
 }
 
-class JsonData{
-
+class JsonData {
   @ApiProperty({
     description: '适配器类型',
-    required:true,
-    default:'page'
+    required: true,
+    default: 'page',
   })
-  type:string
+  type: string;
 
   @ApiProperty({
     description: '适配器标识名称',
-    required:true,
+    required: true,
+    default: 'org',
   })
-  name:string
+  name: string;
 
   @ApiProperty({
     description: '对应组件枚举 eg:matrix_general',
-    required:true,
+    required: true,
+    default: 'org_general',
   })
-  componentName:string
+  componentName: string;
 
   @ApiProperty({
-    description: '入口后缀 eg:Matrix',
-    required:true,
-    default:''
+    description: '入口后缀',
+    required: true,
+    default: 'OrgGraph',
   })
-  entrySuffixName:string
+  entrySuffixName: string;
 
   @ApiProperty({
     description: '组件要素',
-    default:[]
+    default: [
+      {
+        field: 'queryInfo',
+        elementName: 'queryList',
+        message: '组织图信息',
+      },
+    ],
   })
-  element: elementDto[]
+  element: elementDto[];
 }
 
 @ApiTags('内部工具')
@@ -68,7 +74,12 @@ export class GenToolController {
   constructor(private readonly genToolService: GenToolService) {}
   @Post('getCodeByJson')
   @ApiOperation({ summary: '通过json获取代码生成内容' })
-  async getCodeByJson(@Body() jsonData:JsonData){
-    return await quickGenAdapter(jsonData)
+  async getCodeByJson(@Body() jsonData: JsonData) {
+    try {
+      await quickGenAdapter(jsonData);
+    } catch (error) {
+      console.log(error);
+    }
+    return await quickGenAdapter(jsonData);
   }
 }
