4903d8afdd49e7a1a3578de07c9785c8d2318c24
MonsterXiong <942849672@qq.com>
2024-03-11 02:33:45 +0800

    update

--- src/modules/extends/gen/adapter/table/mergeTableAdapter.ts
+++ src/modules/extends/gen/adapter/table/mergeTableAdapter.ts
@@ -1,8 +1,30 @@
+import { camelCase, pascalCase } from "change-case"
+
 export function mergeTableAdapter(param){
   const { name,pageName, detailParam } = param
   const  { templateParam } = detailParam
-  // if(!templateParam || !Object.keys(templateParam)?.length){
-  //   return null
-  // }
-  return param
+  if(!templateParam || !Object.keys(templateParam)?.length){
+    return null
+  }
+  const { tableCode:tableKey,attrs,tableName } = templateParam
+  const primaryKey = attrs?.find(item=>!!item.isPrimaryKey)?.prop || 'id'
+  const tableCode = pascalCase(tableKey)
+  const {tableList} = attrs.reduce((pre,cur)=>{
+    cur.prop = camelCase(cur.prop)
+    if(!cur.isHidden){
+      pre['tableList'].push(cur)
+    }
+    return pre
+  },{tableList:[]})
+
+  const entry = {
+    list:tableList,
+    tableCode
+  }
+
+  return {
+    name,
+    pageName,
+    entry
+  }
 }
