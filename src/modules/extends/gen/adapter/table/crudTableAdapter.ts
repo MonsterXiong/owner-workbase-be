e58b73c8ac8b7a492a4eed7b580d55c7e8c8e796
MonsterXiong <942849672@qq.com>
2024-03-13 22:51:29 +0800

    crud完成

--- src/modules/extends/gen/adapter/table/crudTableAdapter.ts
+++ src/modules/extends/gen/adapter/table/crudTableAdapter.ts
@@ -1,4 +1,5 @@
 import { camelCase, pascalCase } from "change-case"
+import { uniqueArray } from "../../../../../../submodule/genCode-utils/src/utils/array"
 
 export function crudTableAdapter(param){
   const { name,pageName, detailParam } = param
@@ -9,7 +10,7 @@ export function crudTableAdapter(param){
   const { tableCode:tableKey,attrs,tableName } = templateParam
   const primaryKey = camelCase(attrs?.find(item=>!!item.isPrimaryKey)?.prop || 'id')
   const tableCode = pascalCase(tableKey)
-  const {queryList,tableList,formList} = attrs.reduce((pre,cur)=>{
+  const {queryList,tableList,dialogList} = attrs.reduce((pre,cur)=>{
     cur.prop = camelCase(cur.prop)
     if(!cur.isHidden){
       pre['tableList'].push(cur)
@@ -18,10 +19,57 @@ export function crudTableAdapter(param){
       pre['queryList'].push(cur)
     }
     if(cur.isForm){
-      pre['formList'].push(cur)
+      pre['dialogList'].push(cur)
     }
     return pre
-  },{queryList:[],tableList:[],formList:[]})
+  }, { queryList: [], tableList: [], dialogList: [] })
+
+
+  const enumList = uniqueArray([...queryList, ...tableList, ...dialogList] as [],'prop').filter(item=>item.displayType == 'select').map(item => {
+    return {
+      prop: item.prop,
+      type: item.configParam.type,
+      param: item.configParam.param,
+      data: item.configParam.data,
+      configParam:item.configParam
+      // tableCode: pascalCase(item.configParam.param.tableName),
+      // valueKey: camelCase(item.configParam.param.valueKey),
+      // labelKey:camelCase(item.configParam.param.labelKey)
+    }
+  })
+
+  const entryEnumList = []
+  const queryEnumList = []
+  const dialogEnumList = []
+  const tableEnumList = []
+  enumList.forEach(enumItem => {
+    const { prop, type, param, data } = enumItem
+    let enumData = enumItem
+    if (type == 'interface') {
+      enumData = {
+        ...enumItem,
+        param: {
+          tableName:param.tableName,
+          pascalCaseProp:pascalCase(prop),
+          pascalCaseTableName:pascalCase(param.tableName),
+          valueKey:camelCase(param.valueKey),
+          labelKey:camelCase(param.labelKey),
+        }
+      }
+      entryEnumList.push(enumData)
+      if (queryList.find(e => e.prop == prop)) {
+        queryEnumList.push(enumData)
+      }
+      if (dialogList.find(e => e.prop == prop)) {
+        dialogEnumList.push(enumData)
+      }
+      if (tableList.find(e => e.prop == prop)) {
+        tableEnumList.push(enumData)
+      }
+    }
+  });
+  console.log('entryEnumList',entryEnumList);
+
   const camelCaseName = camelCase(name)
 
   const query = {
@@ -31,24 +79,32 @@ export function crudTableAdapter(param){
         ...item,
         formDataCode:'queryForm'
       }
-    })
+    }),
+    enumList:queryEnumList
   }
   const dialog = {
     tableName,
-    list:formList.map(item=>{
+    primaryKey,
+    list:dialogList.map(item=>{
       return {
         ...item,
         formDataCode:'formData'
       }
-    })
+    }),
+    enumList:dialogEnumList
   }
   const table = {
-    list:tableList
+    list: tableList,
+    enumList:tableEnumList
   }
   const entry = {
     camelCaseName,
     primaryKey,
-    tableCode
+    tableCode,
+    enumList: entryEnumList,
+    dialogEnumList,
+    tableEnumList,
+    queryEnumList
   }
   return {
     name,
