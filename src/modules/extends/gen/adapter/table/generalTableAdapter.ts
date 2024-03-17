b27be93b9d51436bb56659da80e2072a224b6f46
MonsterXiong <942849672@qq.com>
2024-03-18 01:59:16 +0800

    update

--- src/modules/extends/gen/adapter/table/generalTableAdapter.ts
+++ src/modules/extends/gen/adapter/table/generalTableAdapter.ts
@@ -2,6 +2,11 @@ export function generalTableAdapter(param){
   // tip:暂时使用的是crudTableAdapter
   const { name,pageName, detailParam } = param
   const  { templateParam } = detailParam
+  console.log('templateParam',templateParam);
+  const { pageConfig } =  templateParam
+  const { isQuery, isForm } = pageConfig
+  console.log(isQuery,'isQuery ');
+
   // if(!templateParam || !Object.keys(templateParam)?.length){
   //   return null
   // }
